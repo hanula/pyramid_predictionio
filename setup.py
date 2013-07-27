b9ffc7e4a4b7c6170e8efc4c3940d942106aa2a7
Sebastian Hanula <sebastian.hanula@gmail.com>
2013-07-27 16:57:47 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+
+setup(
+    name='pypkg_template',
+    version='0.1.0',
+    description='Python package template.',
+    long_description=readme,
+
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.3',
+    ],
+    author='Sebastian Hanula',
+    author_email='sebastian.hanula@gmail.com',
+    url='https://github.com/hanula/pypkg_template',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points="""
+      # -*- Entry points: -*-
+    """,
+)
